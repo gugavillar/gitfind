2ff9ee64b9173b8f8b2e58991ef442504075f4bd
Guga Villar <grsv.21@gmail.com>
2023-10-26 14:53:20 -0300

    Upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -16,5 +18,20 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ theme, addUtilities }) => {
+      const neonUtilities = {}
+      const colors = theme('colors')
+      for (const color in colors) {
+        if (typeof colors[color] === 'object') {
+          const color1 = colors[color]['500']
+          const color2 = colors[color]['700']
+          neonUtilities[`.neon-${color}`] = {
+            boxShadow: `0 0 5px ${color1}, 0 0 10px ${color2}`,
+          }
+        }
+      }
+      addUtilities(neonUtilities)
+    }),
+  ],
 }
